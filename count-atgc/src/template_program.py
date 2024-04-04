d9f39d7c391d175ccc6fdfef93443a2815d931c9
ximenakgp <ximenagp@lcg.unam.mx>
2024-04-04 06:30:42 -0600

    Se creó un programa para contar el contenido de nucleótidos en una secuenica de DNA.

--- count-atgc/src/template_program.py
+++ count-atgc/src/template_program.py
@@ -1,75 +1,40 @@
-'''
-NAME: 
+"""
+NAME:Contenido de As, Ts, Gs, Cs
        
-
-VERSION:
-        
+VERSION:1
 
 AUTHOR: KARLA XIMENA GONZALEZ PLATAS
 
-        
-
-DESCRIPTION: 
-        
+DESCRIPTION: Programa que calcula el contenido de cada nucleótido en una secuencia de DNA. 
+La secuencia viene en un archivo de texto en formato raw llamado sequence.txt. Imprimir en pantalla el resultado de los conteos.
 
 CATEGORY
-        
-
+    
 USAGE
 
-    % python programName
+    % python count_atgc.py
     
-
 ARGUMENTS
-
 
 METHOD
 
+"""
 
-SEE ALSO
+# Abrir el archivo con la secuencia de ADN
+with open('sequence.txt', 'r') as file:
+    sequence = file.read().strip()
 
+# Convertir la secuencia a mayúsculas para contar sin distinción de mayúsculas y minúsculas
+sequence = sequence.upper()
 
-        
-'''
+# Inicializar contadores
+count_A = sequence.count('A')
+count_C = sequence.count('C')
+count_G = sequence.count('G')
+count_T = sequence.count('T')
 
-
-# ===========================================================================
-# =                            imports
-# ===========================================================================
-
-
-
-
-
-# ===========================================================================
-# =                            Command Line Options
-# ===========================================================================
-
-
-
-
-
-
-# ===========================================================================
-# =                            functions
-# ===========================================================================
-
-
-
-
-
-# ===========================================================================
-# =                            main
-# ===========================================================================
-
-
-# step 1.
-
-
-# step 2.
-
-
-# step 3.
-
-
-
+# Imprimir los resultados
+print(f'Número de A: {count_A}')
+print(f'Número de C: {count_C}')
+print(f'Número de G: {count_G}')
+print(f'Número de T: {count_T}')
